4b323f0de5413d1dafe96e339c0c14d8e26d9c89
Rodrigo <rodrigo.santos@criaschool.com>
2022-07-12 17:54:04 -0300

    feat: finalizado filter

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -8,13 +8,15 @@ export const Header = () => {
           <div className="basis-4/4 cursor-pointer hover:text-yellow  text-grey m-2">
             Home
           </div>
-          <div className="basis-4/4 hover:text-yellow text-grey m-2">
+          <div className="basis-4/4 cursor-pointer hover:text-yellow text-grey m-2">
             Streams
           </div>
-          <div className="basis-4/4 hover:text-yellow text-grey m-2">
+          <div className="basis-4/4 cursor-pointer hover:text-yellow text-grey m-2">
             Game Store
           </div>
-          <div className="basis-4/4 hover:text-yellow text-grey m-2">News</div>
+          <div className="basis-4/4 cursor-pointer hover:text-yellow text-grey m-2">
+            News
+          </div>
         </div>
       </header>
     </>
--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,27 +1,30 @@
 import React from "react";
-
+import { FiLinkedin, FiInstagram } from "react-icons/fi";
 export const SideBar = () => {
   const categories = ["Indy", "Adventure", "MMO", "Casual game"];
   const plataforms = ["PC", "PlayStation", "Xbox", "Nintendo"];
   return (
     <nav>
       <div className="bg-grey-heavy opacity-75 p-5 rounded-lg ml-5 w-72 mt-8 ">
-        <h1 className="text-white ml-5  ">Categories</h1>
+        <h1 className="text-[#fbfbfb] ml-5  ">Categories</h1>
         <div className="text-grey mt-2 text-md ml-5 ">
           {categories.map((categorie) => (
             <div key={categorie}>
-              <input type="checkbox" />
+              <input
+                className="accent-yellow cursor-pointer text-white"
+                type="checkbox"
+              />
               <label className="ml-2 font-light" htmlFor="">
                 {categorie}
               </label>
             </div>
           ))}
         </div>
-        <h1 className="text-white ml-5 mt-10 ">Plataform</h1>
+        <h1 className="text-[#fbfbfb] ml-5 mt-10 ">Plataform</h1>
         <div className="text-grey mt-2 text-md ml-5 ">
           {plataforms.map((plataform) => (
             <div key={plataform}>
-              <input type="checkbox" />
+              <input className="accent-yellow cursor-pointer" type="checkbox" />
               <label className="ml-2 font-light" htmlFor="">
                 {plataform}
               </label>
@@ -31,6 +34,18 @@ export const SideBar = () => {
         <button className="bg-yellow font-medium p-2 rounded-xl w-full  mt-10">
           Apply Filters
         </button>
+      </div>
+      <div className="flex flex-row ml-24">
+        <div className="basis-4/4 m-4">
+          <a href="https://www.linkedin.com/in/rodrdev/">
+            <FiLinkedin className="text-grey text-2xl"></FiLinkedin>
+          </a>
+        </div>
+        <div className="basis-4/4 m-4">
+          <a href="https://www.instagram.com/rodrigo.dev1/">
+            <FiInstagram className="text-grey text-2xl"></FiInstagram>
+          </a>
+        </div>
       </div>
     </nav>
   );
